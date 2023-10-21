505086fe5aa34c2f97923814f0c7b505ab60f051
Tom Allen <tom@waggel.co.uk>
2023-10-21 18:27:49 +0100

    setup mongo and connection function, need to abstract the connection to model

--- app/view-shifts/page.tsx
+++ app/view-shifts/page.tsx
@@ -1,7 +1,32 @@
 import { FunctionComponent } from "react";
 import ViewShiftsPage from "Pages/ViewShifts/ViewShiftsPage";
 
-const ViewShiftsRoute: FunctionComponent = () => {
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.CONNECTION_URL;
+const client = new MongoClient(uri!, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const getWorkerTypes = async () => {
+  try {
+    await client.connect();
+    await client.db("sead").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+};
+
+const ViewShiftsRoute: FunctionComponent = async () => {
+  await getWorkerTypes();
   return <ViewShiftsPage />;
 };
 
